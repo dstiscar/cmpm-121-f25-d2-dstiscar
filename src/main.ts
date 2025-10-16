62110aa4e2e95b87008819177e104552fad33812
dstiscar <dstiscar@ucsc.edu>
2025-10-16 00:56:30 +0000

    Step 2 Complete: implement drawing and a clear button

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,43 @@
 import "./style.css";
 
 document.body.innerHTML = `
-  <h1>Draw Canvas</h1>
-  <canvas id="canvas"></canvas>
+<h1>Drawing with mouse events</h1>
+<canvas id="myCanvas" width="256" height="256"></canvas>
+<br><br>
+<button id="clearButton">clear</button>
 `;
 
-const canvas = document.createElement("canvas");
-document.body.append(canvas);
+let isDrawing = false;
+let x = 0;
+let y = 0;
+
+const myCanvas = document.getElementById("myCanvas");
+const ctx = myCanvas.getContext("2d");
+const clearButton = document.getElementById("clearButton");
+
+myCanvas.addEventListener("mousedown", (e) => {
+  x = e.offsetX;
+  y = e.offsetY;
+  isDrawing = true;
+});
+
+myCanvas.addEventListener("mousemove", (e) => {
+  if (isDrawing) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    x = e.offsetX;
+    y = e.offsetY;
+  }
+});
+
+window.addEventListener("mouseup", (e) => {
+  if (isDrawing) {
+    isDrawing = false;
+  }
+});
+
+clearButton.addEventListener("click", () => {
+  ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+});
