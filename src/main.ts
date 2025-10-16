9136dc53054a9c7b0edf42fb0f41c28ed8407aff
dstiscar <dstiscar@ucsc.edu>
2025-10-16 02:05:18 +0000

    Step 4 Complete: add undo and redo buttons

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,23 @@ document.body.innerHTML = `
 <canvas id="myCanvas" width="256" height="256"></canvas>
 <br><br>
 <button id="clearButton">clear</button>
+<button id="undoButton">undo</button>
+<button id="redoButton">redo</button>
 `;
 
 const myCanvas = document.getElementById("myCanvas") as HTMLCanvasElement;
 const ctx = myCanvas.getContext("2d") as CanvasRenderingContext2D;
 const clearButton = document.getElementById("clearButton") as HTMLCanvasElement;
+const undoButton = document.getElementById("undoButton") as HTMLCanvasElement;
+const redoButton = document.getElementById("redoButton") as HTMLCanvasElement;
 
 const redrawEvent = new Event("redraw");
 
 type Point = { x: number; y: number };
 type Line = Point[];
 const lines: Line[] = [];
-let currentLine: Line | null = null;
+const redoLines: Line[] = [];
+let currentLine: Line = [];
 
 let isDrawing = false;
 let cx = 0;
@@ -45,6 +50,7 @@ myCanvas.addEventListener("mousedown", (e) => {
   currentLine = [];
   lines.push(currentLine);
   currentLine.push({ x: cx, y: cy });
+  redoLines.splice(0, redoLines.length);
 
   myCanvas.dispatchEvent(redrawEvent);
 });
@@ -53,20 +59,38 @@ myCanvas.addEventListener("mousemove", (e) => {
   if (isDrawing) {
     cx = e.offsetX;
     cy = e.offsetY;
-    if (currentLine) {
-      currentLine.push({ x: cx, y: cy });
-    }
+    currentLine.push({ x: cx, y: cy });
     myCanvas.dispatchEvent(redrawEvent);
   }
 });
 
 myCanvas.addEventListener("mouseup", () => {
   isDrawing = false;
-  currentLine = null;
+  currentLine = [];
   myCanvas.dispatchEvent(redrawEvent);
 });
 
 clearButton.addEventListener("click", () => {
   lines.splice(0, lines.length);
   myCanvas.dispatchEvent(redrawEvent);
+});
+
+undoButton.addEventListener("click", () => {
+  if (lines.length > 0) {
+    const popLine = lines.pop();
+    if (popLine) {
+      redoLines.push(popLine);
+    }
+    myCanvas.dispatchEvent(redrawEvent);
+  }
+});
+
+redoButton.addEventListener("click", () => {
+  if (redoLines.length > 0) {
+    const popLine = redoLines.pop();
+    if (popLine) {
+      lines.push(popLine);
+    }
+    myCanvas.dispatchEvent(redrawEvent);
+  }
 });
